0877a9ba3e9cfc66c5247754f7042ef2ccfc6c38
Phillip Kruger <phillip.kruger@gmail.com>
2022-06-06 17:18:09 +1000

    bla
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- client/src/main/java/pt/jnation/blockbuster/model/MovieReference.java
+++ client/src/main/java/pt/jnation/blockbuster/model/MovieReference.java
@@ -5,5 +5,10 @@ public class MovieReference {
 
     public String id;
     public String title;
+
+    @Override
+    public String toString() {
+        return "MovieReference{" + "id=" + id + ", title=" + title + '}';
+    }
     
 }
