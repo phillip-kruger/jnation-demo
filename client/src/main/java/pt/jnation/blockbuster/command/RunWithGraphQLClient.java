26419009d86f34ef924d4425dbe689580f6405ba
Jan Martiska <jmartisk@redhat.com>
2022-06-07 08:42:14 +0200

    minor client changes

--- client/src/main/java/pt/jnation/blockbuster/command/RunWithGraphQLClient.java
+++ client/src/main/java/pt/jnation/blockbuster/command/RunWithGraphQLClient.java
@@ -21,7 +21,7 @@ import static io.smallrye.graphql.client.core.Document.document;
 import static io.smallrye.graphql.client.core.Field.field;
 import static io.smallrye.graphql.client.core.Operation.operation;
 
-@CommandLine.Command(name = "graphql", description = "Use the JAXRS REST Client")
+@CommandLine.Command(name = "graphql", description = "Use the GraphQL Client")
 public class RunWithGraphQLClient implements Runnable {
 
     @CommandLine.Option(names = "-o", description = "The operation to execute")
